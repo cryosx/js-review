3364915bd0d75aeb65500ce13cc19a058a750a1b
cryosx <cyruswu.email@gmail.com>
2018-02-28 19:51:27 -1000

    ex4

--- exercises.js
+++ exercises.js
@@ -41,17 +41,34 @@ console.log(drinksGin);
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
 
+var completedCodingChallenge = null;
+var traveledToSpace = null;
+
+console.log(completedCodingChallenge);
+console.log(traveledToSpace);
+
 /*5.  Variables with Array for its value.
 Declare a variable named japanPrefectures and assign the value of any 5 prefectures in Japan.
 Console log the variable 
 Console log the value at index 1.
 Console log the value at index 3.*/
 
+var japanPrefectures = ["Tokyo", "Aomori", "Akita", "Oita", "Mie"];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
+
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
 Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
+
+var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+console.log(planets);
+console.log(planets[2]);
+console.log(planets[6]);
 
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.  
